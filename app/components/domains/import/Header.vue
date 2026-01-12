e440aa01cc6c4da34cb8343fca0dfc7a8496c23c
wallcreation <lanhajames@gmail.com>
2026-01-12 21:55:00 +0100

    Add domain import form for testing and button to redirect to payment

---
+++ app/components/domains/import/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <UContainer>
+    <div class="flex items-center gap-3">
+      <UIcon name="i-heroicons-download" class="size-6 text-primary-600" />
+      <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">
+        Import domain
+      </h1>
+    </div>
+  </UContainer>
+</template>
+
+<script lang="ts" setup>
+// Pas de logique pour l’instant
+</script>
