cdc1ad304fdc3e8656942ca622078ab03254fd75
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2026-01-24 20:36:56 +0100

    logo and title updted

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Existantly - Cold Email Platform",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/logo.webp" }],
+      meta: [{ property: "og:image", content: "/logo.webp" }],
+    },
+  },
   runtimeConfig: {
     public: {
       apiUrl: process.env.API_BASE,
